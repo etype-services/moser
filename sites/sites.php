0b6e484d3e7d92732ebc15bcc78f9a3d6e003ff3
etype <etype@bluehost.c.modern-photon-112115.internal>
2016-01-21 18:35:42 +0000

    initial commit

---
+++ sites/sites.php
@@ -0,0 +1,46 @@
+<?php
+
+$sites['bosquecountytoday.etypegoogle4.com'] = 'bosquecountytoday.com';
+$sites['bosquecountytoday.com'] = 'bosquecountytoday.com';
+$sites['www.bosquecountytoday.com'] = 'bosquecountytoday.com';
+$sites['prod.bosquecountytoday.com'] = 'bosquecountytoday.com';
+$sites['dev.bosquecountytoday.com'] = 'bosquecountytoday.com';
+
+$sites['cliftonrecord.com'] = 'bosquecountytoday.com';
+$sites['www.cliftonrecord.com'] = 'bosquecountytoday.com';
+$sites['prod.cliftonrecord.com'] = 'bosquecountytoday.com';
+$sites['dev.cliftonrecord.com'] = 'bosquecountytoday.com';
+
+$sites['meridiantribune.com'] = 'bosquecountytoday.com';
+$sites['www.meridiantribune.com'] = 'bosquecountytoday.com';
+$sites['prod.meridiantribune.com'] = 'bosquecountytoday.com';
+$sites['dev.meridiantribune.com'] = 'bosquecountytoday.com';
+
+
+$sites['dewittcountytoday.etypegoogle4.com'] = 'dewittcountytoday.com';
+$sites['dewittcountytoday.com'] = 'dewittcountytoday.com';
+$sites['www.dewittcountytoday.com'] = 'dewittcountytoday.com';
+$sites['prod.dewittcountytoday.com'] = 'dewittcountytoday.com';
+$sites['dev.dewittcountytoday.com'] = 'dewittcountytoday.com';
+
+$sites['yorktownnews-view.com'] = 'dewittcountytoday.com';
+$sites['www.yorktownnews-view.com'] = 'dewittcountytoday.com';
+$sites['prod.yorktownnews-view.com'] = 'dewittcountytoday.com';
+$sites['dev.yorktownnews-view.com'] = 'dewittcountytoday.com';
+
+$sites['cuerorecord.com'] = 'dewittcountytoday.com';
+$sites['www.cuerorecord.com'] = 'dewittcountytoday.com';
+$sites['prod.cuerorecord.com'] = 'dewittcountytoday.com';
+$sites['dev.cuerorecord.com'] = 'dewittcountytoday.com';
+
+
+$sites['yourforneymessenger.etypegoogle4.com'] = 'yourforneymessenger.com';
+$sites['yourforneymessenger.com'] = 'yourforneymessenger.com';
+$sites['www.yourforneymessenger.com'] = 'yourforneymessenger.com';
+$sites['prod.yourforneymessenger.com'] = 'yourforneymessenger.com';
+$sites['dev.yourforneymessenger.com'] = 'yourforneymessenger.com';
+
+$sites['forneymessengernewspaper.com'] = 'yourforneymessenger.com';
+$sites['www.forneymessengernewspaper.com'] = 'yourforneymessenger.com';
+$sites['prod.forneymessengernewspaper.com'] = 'yourforneymessenger.com';
+$sites['dev.forneymessengernewspaper.com'] = 'yourforneymessenger.com';
