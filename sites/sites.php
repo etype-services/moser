d124621eb9fb5663e01b4ef555c53eb9e43f67b3
etype <etype@moser.c.modern-photon-112115.internal>
2016-01-22 16:11:55 +0000

    sites.php

--- sites/sites.php
+++ sites/sites.php
@@ -32,15 +32,3 @@ $sites['cuerorecord.com'] = 'dewittcountytoday.com';
 $sites['www.cuerorecord.com'] = 'dewittcountytoday.com';
 $sites['prod.cuerorecord.com'] = 'dewittcountytoday.com';
 $sites['dev.cuerorecord.com'] = 'dewittcountytoday.com';
-
-
-$sites['yourforneymessenger.etypegoogle4.com'] = 'yourforneymessenger.com';
-$sites['yourforneymessenger.com'] = 'yourforneymessenger.com';
-$sites['www.yourforneymessenger.com'] = 'yourforneymessenger.com';
-$sites['prod.yourforneymessenger.com'] = 'yourforneymessenger.com';
-$sites['dev.yourforneymessenger.com'] = 'yourforneymessenger.com';
-
-$sites['forneymessengernewspaper.com'] = 'yourforneymessenger.com';
-$sites['www.forneymessengernewspaper.com'] = 'yourforneymessenger.com';
-$sites['prod.forneymessengernewspaper.com'] = 'yourforneymessenger.com';
-$sites['dev.forneymessengernewspaper.com'] = 'yourforneymessenger.com';
