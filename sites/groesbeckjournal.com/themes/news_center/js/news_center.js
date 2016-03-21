ca6e28eb353b0edbf81b37b4c80b9de4292db917
Charlie Llewellin <charliellewellin@gmail.com>
2016-03-21 11:13:08 -0500

    groesbeck/moser zen update

--- sites/groesbeckjournal.com/themes/news_center/js/news_center.js
+++ sites/groesbeckjournal.com/themes/news_center/js/news_center.js
@@ -81,7 +81,7 @@
 
             $(window).resize(function(){
                 var w = $(window).width();
-                if (w > 979) {
+                if (w > 767) {
                     $('#block-superfish-1 ul li:not(:first-child)').css("display", "inline-block").show();
                 } else {
                     $('#block-superfish-1 ul li:first-child a').text('Show Menu');
