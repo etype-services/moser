ba4e59ecf7a3ae175a0f0fdc1dbd758d2873324b
Charlie Llewellin <charliellewellin@gmail.com>
2016-04-04 19:33:57 -0500

    graham leader

--- sites/grahamleader.com/themes/cni/template.php
+++ sites/grahamleader.com/themes/cni/template.php
@@ -28,6 +28,10 @@ function cni_preprocess_node(&$variables) {
   if (!empty($node->classes_array)) {
     $variables['classes_array'] = array_merge($variables['classes_array'], $node->classes_array);
   }
+
+  if (node_is_page($node) !== false) {
+    drupal_add_js('//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-56e774978692f861', 'external');
+  }
 }
 
 /* Breadcrumbs */
