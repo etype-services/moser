a95210596e9990445616cf374de58e9c46ca1be2
Charlie Llewellin <charliellewellin@gmail.com>
2016-04-14 14:30:02 -0500

    hennessey clipper

--- sites/hennesseyclipper.com/themes/cni/template.php
+++ sites/hennesseyclipper.com/themes/cni/template.php
@@ -79,13 +79,4 @@ function cni_preprocess_html(&$vars) {
     $vars['classes_array'][] = drupal_clean_css_identifier($alias);
   }
 
-  /*
-   * Hide Log In link from logged in users
-   * A call to hook_menu_alter in etype.module does not do the trick
-   */
-  global $user;
-  if ($user->uid > 0) {
-    drupal_add_js('sites/all/themes/cni/js/user-menu.js', 'file');
-  }
-
 }
