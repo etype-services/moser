5f8679833e4c04dec1d07b7c5375071f6e3a2a71
Charlie Llewellin <charliellewellin@gmail.com>
2016-05-13 20:23:46 -0500

    aransas pass

--- sites/aransaspassprogress.com/themes/cni/templates/page--front.tpl.php
+++ sites/aransaspassprogress.com/themes/cni/templates/page--front.tpl.php
@@ -5,11 +5,6 @@
             <nav id="user-menu" class="clearfix">
                 <?php print render($page['user_menu']); ?>
                 <?php print render($page['search_box']); ?>
-                <div id="social">
-                    <ul class="social-links">
-                        <li><a class="facebook" href="<?php echo $facebook ?>"></a></li>
-                    </ul>
-                </div>
             </nav>
         <?php endif; ?>
 
