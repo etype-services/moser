81f08fae7904b94b219e304aa29506a10210ff86
Charlie Llewellin <charliellewellin@gmail.com>
2016-05-19 17:19:42 -0500

    fredericksburg cni

--- sites/cni-theme-test/themes/cni/js/scripts.js
+++ sites/cni-theme-test/themes/cni/js/scripts.js
@@ -51,15 +51,11 @@
     Drupal.behaviors.superfish = {
         attach: function (context) {
             var obj = $('#block-superfish-1 ul li.sf-depth-1:first-child a');
+            $(this).text('');
             obj.click(function(e){
                 var text = $(this).text();
                 e.preventDefault();
                 $('#block-superfish-1 ul li.sf-depth-1:not(:first-child)').toggle();
-                if (text == 'Show Menu') {
-                    $(this).text('Hide Menu');
-                } else {
-                    $(this).text('Show Menu');
-                }
             });
 
             $(window).resize(function(){
@@ -67,7 +63,6 @@
                 if (w > 767) {
                     $('#block-superfish-1 ul li.sf-depth-1:not(:first-child)').css("display", "list-item").show();
                 } else {
-                    $('#block-superfish-1 ul li.sf-depth-1:first-child a').text('Show Menu');
                     $('#block-superfish-1 ul li.sf-depth-1:not(:first-child)').hide();
                     $('#block-superfish-1 ul li.sf-depth-1 ul').hide();
                 }
