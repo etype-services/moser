cc31503eb603028fae0e44b550ff9dc45bbf946d
Charlie Llewellin <charliellewellin@gmail.com>
2016-06-02 11:57:21 -0500

    new etype (cni) theme

--- sites/all/themes/etype/template.php
+++ sites/all/themes/etype/template.php
@@ -3,6 +3,34 @@
 /* First and Last Classes on Teasers */
 
 function etype_preprocess_page(&$variables) {
+	$grid_info = get_grid_info();
+	
+	// Create page variables
+	
+	$vars['grid_size'] = 'container_' . $grid_info['grid_size'];
+	$vars['grid_full_width'] = 'grid_' . $grid_info['grid_size'];
+	$vars['sidebar_first_grid_width'] = 'grid_' . $grid_info['sidebar_first_width'];
+  $vars['sidebar_second_grid_width'] = 'grid_' . $grid_info['sidebar_second_width'];
+	$vars['twitter'] = theme_get_setting('twitter');
+  $vars['facebook'] = theme_get_setting('facebook');
+  $vars['pinterest'] = theme_get_setting('pinterest');
+
+	for ($region_count = 1; $region_count <= 4; $region_count++) {
+	  $vars['preface_' . $region_count . '_grid_width'] = 'grid_' . $grid_info['preface_' . $region_count . '_grid_width'];
+		$vars['postscript_' . $region_count . '_grid_width'] = 'grid_' . $grid_info['postscript_' . $region_count . '_grid_width'];
+	}	
+
+	if (empty($vars['page']['sidebar_first']) && empty($vars['page']['sidebar_second'])) {
+		$vars['main_content_grid_width'] = 'grid_' . $grid_info['grid_size'];
+	} else if (!empty($vars['page']['sidebar_first']) && !empty($vars['page']['sidebar_second'])) {
+		$vars['main_content_grid_width'] = 'grid_' . ($grid_info['grid_size'] - ($grid_info['sidebar_first_width'] + $grid_info['sidebar_second_width']));
+ 	} else if (empty($vars['page']['sidebar_first']) && !empty($vars['page']['sidebar_second'])) {
+		$vars['main_content_grid_width'] = 'grid_' . ($grid_info['grid_size'] - $grid_info['sidebar_second_width']);
+	} else if (!empty($vars['page']['sidebar_first']) && empty($vars['page']['sidebar_second'])) {
+		$vars['main_content_grid_width'] = 'grid_' . ($grid_info['grid_size'] - $grid_info['sidebar_first_width']);
+	}
+
+
   if (isset ($variables['page']['content']['system_main']['nodes'])) {
     $nodes = $variables['page']['content']['system_main']['nodes'];
     $i = 1;
@@ -71,6 +99,8 @@ function etype_form_alter(&$form, &$form_state, $form_id) {
  * @param $vars
  */
 function etype_preprocess_html(&$vars) {
+
+$vars['classes_array'][] = theme_get_setting('sidebar_layout');
 
   /* Add Page Body Class */
   $path = drupal_get_path_alias($_GET['q']);
