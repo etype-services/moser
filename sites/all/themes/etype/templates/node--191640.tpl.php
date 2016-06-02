f362a6c5c54ab4058163bd831f68f0a3979833ae
Charlie Llewellin <charliellewellin@gmail.com>
2016-06-02 10:33:37 -0500

    new etype (cni) theme

--- sites/all/themes/etype/templates/node--191640.tpl.php
+++ sites/all/themes/etype/templates/node--191640.tpl.php
@@ -75,19 +75,7 @@ if($Password==$resp->GetPasswordByUserNameResult)
 
      <p style="color:red"><?php echo $msg; ?></p>
        <!-- <h4  style="background:gray;line-height: 2.0em;font-size: 16px;"><center>My Account</center></h4>-->
-      
-     
-            <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
-                          <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
-                          <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
-                          
-                          <script>
-                          $(function() {
-                            $( "#tabs" ).tabs();
-                          });
-                          </script>
-                        </head>
-                        <body>
+
                          
                         <div id="tabs">
                           <ul>
