63b758f5b436f752e9101a2533d86b5c3644c374
Charlie Llewellin <charliellewellin@gmail.com>
2016-06-15 11:58:27 -0500

    Merge branch 'master' into new_theme
    * master:
      new theme menu width
      new theme menu width
      new theme menu width
      new theme menu width
      update premium module
      graham leader
      graham leader
      graham leader
      colorbox  update
    
    Conflicts:
    	sites/all/modules/etype/etype.module

--- sites/fred-cni/themes/cni/templates/page--front.tpl.php
+++ sites/fred-cni/themes/cni/templates/page--front.tpl.php
@@ -14,8 +14,9 @@
 
                     <?php if ($logo): ?>
                         <div class="site-logo">
-                        <a href="<?php print check_url($front_page); ?>"><img src="<?php print $logo ?>"
-                                                                              alt="<?php print $site_name; ?>"/></a>
+                        <a href="<?php print check_url($front_page); ?>"><img
+                                src="<?php print $logo ?>"
+                                alt="<?php print $site_name; ?>"/></a>
                         </div><?php print render($page['header']) ?>
                     <?php endif; ?>
 
@@ -27,25 +28,15 @@
     </div>
 </div>
 
-        <?php if ($page['main_menu']): ?>
-            <div class="main-menu-wrapper clearfix">
-                <div class="main-menu-wrapper-inner">
-                    <nav id="main-menu">
-                        <?php print render($page['main_menu']); ?>
-                    </nav>
-                </div>
-            </div>
-        <?php endif; ?>
-
-        <?php if ($page['main_menu_second_level']): ?>
-            <div class="main-menu-second-level-wrapper clearfix">
-                <div class="main-menu-second-level-wrapper-inner">
-                    <nav id="main-menu-second-level">
-                        <?php print render($page['main_menu_second_level']); ?>
-                    </nav>
-                </div>
-            </div>
-        <?php endif; ?>
+<?php if ($page['main_menu']): ?>
+    <div class="main-menu-wrapper clearfix">
+        <div class="main-menu-wrapper-inner">
+            <nav id="main-menu">
+                <?php print render($page['main_menu']); ?>
+            </nav>
+        </div>
+    </div>
+<?php endif; ?>
 
 <div id="page">
     <div class="page-inner <?php echo $grid_size ?>">
@@ -73,7 +64,8 @@
                                 <div class="preface-wrapper
                                 preface-wrapper-top clearfix">
                                     <div class="preface-wrapper-inner">
-                                        <div class="preface-wrapper-inner-inner">
+                                        <div
+                                            class="preface-wrapper-inner-inner">
                                             <section id="preface_1">
                                                 <div><?php print render
                                                     ($page['preface_1']); ?></div>
@@ -83,52 +75,37 @@
                                 </div>
                             <?php endif; ?>
 
+                            <?php print render($page['content_middle']); ?>
+
+                            <?php if ($page['preface_2'] || $page['preface_3']): ?>
+                                <div class="preface-wrapper clearfix">
+                                    <div class="preface-wrapper-inner">
+                                        <div
+                                            class="preface-wrapper-inner-inner">
+                                            <section id="preface">
+                                                <div><?php print render($page['preface_2']); ?></div>
+                                                <div><?php print render($page['preface_3']); ?></div>
+                                            </section>
+                                        </div>
+                                    </div>
+                                </div>
+                            <?php endif; ?>
+
+
                         </div>
-                        <?php if ($page['sidebar_first']): ?>
-                            <aside class="sidebar first-sidebar grid_4 clearfix">
+
+                        <?php if ($page['sidebar_second']): ?>
+                            <aside class="sidebar second-sidebar grid_4 clearfix">
                                 <?php print render($page['sidebar_first']); ?>
+                                <?php print render($page['sidebar_second']); ?>
                             </aside>
                         <?php endif; ?>
                     </div>
 
-                    <!-- Main 2 Editor's Pick-->
-                    <div class="main clearfix">
-                        <div class="main-inner grid_12">
-                            <?php print render($page['content_middle']); ?>
-                        </div>
-
-
-                        <!-- Main 3 -->
-                        <div class="main clearfix">
-                            <div class="main-inner grid_8">
-
-                                <?php if ($page['preface_2'] || $page['preface_3']): ?>
-                                    <div class="preface-wrapper clearfix">
-                                        <div class="preface-wrapper-inner">
-                                            <div class="preface-wrapper-inner-inner">
-                                                <section id="preface">
-                                                    <div><?php print render($page['preface_2']); ?></div>
-                                                    <div><?php print render($page['preface_3']); ?></div>
-                                                </section>
-                                            </div>
-                                        </div>
-                                    </div>
-                                <?php endif; ?>
-
-
-                            </div>
-
-                            <?php if ($page['sidebar_second']): ?>
-                                <aside class="sidebar second-sidebar grid_4 clearfix">
-                                    <?php print render($page['sidebar_second']); ?>
-                                </aside>
-                            <?php endif; ?>
-                        </div>
-
                 </section>
             </div>
         </div>
-        
+
         <?php print render($page['content_bottom']); ?>
 
         <?php if ($page['postscript_1'] || $page['postscript_2'] || $page['postscript_3'] || $page['postscript_4']): ?>
