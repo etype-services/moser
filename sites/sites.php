0c17546d9244c22602b90ff0278932deaeef8583
Charlie Llewellin <charliellewellin@gmail.com>
2016-07-07 07:15:42 -0500

    san marcos

--- sites/sites.php
+++ sites/sites.php
@@ -211,6 +211,3 @@ $sites['sanmarcosrecord.com'] = 'sanmarcosdailyrecord.com';
 
 /* test.etypegoogle4.com */
 $sites['test.etypegoogle4.com'] = 'cni-theme-test';
-
-/* smugmug.etypegoogle4.com.conf  */
-$sites['smugmug.etypegoogle4.com'] = 'fredericksburgstandard.com';
