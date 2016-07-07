f424eb2124d817047ea60b767a53dbf429007ee0
Charlie Llewellin <charliellewellin@gmail.com>
2016-07-07 15:57:26 -0500

    san marcos ckeditor

--- sites/sanmarcosdailyrecord.com/themes/masthead/templates/node.tpl.php
+++ sites/sanmarcosdailyrecord.com/themes/masthead/templates/node.tpl.php
@@ -8,7 +8,7 @@
   <?php else: ?>
     <?php if ($teaser): ?>
     <div class="field-image">
-      <img class="teaser-default" src="/sites/sanmarcosdailyrecord.com/files/teaser_default.png" />
+      <div class="field-name-field-image"><img class="teaser-default" src="/sites/sanmarcosdailyrecord.com/files/teaser_default.png" /></div>
     </div>
     <?php endif; ?>
 
