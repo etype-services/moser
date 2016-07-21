7f207ed25d1cc28404a1da2cd43c8b746b4a4b78
Charlie Llewellin <charlie@llewellin.net>
2016-07-21 16:01:32 -0500

    san marcos site name

--- sites/sanmarcosdailyrecord.com/themes/masthead/template.php
+++ sites/sanmarcosdailyrecord.com/themes/masthead/template.php
@@ -82,5 +82,5 @@ function masthead_preprocess_html(&$vars) {
   $site_name = variable_get('site_name', "San Marcos Record");
   drupal_set_title($title . $site_name);
   global $user;
-  if ($user->uid == 1) var_dump($variables['page']);
+  if ($user->uid == 1) var_dump($vars);
 }
