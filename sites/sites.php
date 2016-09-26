4fd829d11113adc1d6d413bcf289a5e5b56a2a83
Charlie Llewellin <charlie@llewellin.net>
2016-09-26 09:45:22 +0100

    marlin & rosebud

--- sites/sites.php
+++ sites/sites.php
@@ -228,6 +228,7 @@ $sites['lakecountryradio.net'] = 'lakecountryradio.net';
 
 /* marlindemocrat.etypegoogle4.com */
 $sites['marlindemocrat.etypegoogle4.com'] = 'marlindemocrat.etypegoogle4.com';
+$sites['marlindemocrat-cni.etypegoogle4.com'] = 'marlindemocrat.etypegoogle4.com';
 
 /* rosebudnews.etypegoogle4.com */
 $sites['rosebudnews.etypegoogle4.com'] = 'rosebudnews.etypegoogle4.com';
