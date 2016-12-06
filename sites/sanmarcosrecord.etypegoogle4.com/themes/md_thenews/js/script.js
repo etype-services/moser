f17e37af4ff9bed1ef948b1ec69cbc68d51467a9
Charlie Llewellin <charlie@llewellin.net>
2016-12-06 13:52:28 -0500

    san marcos css

--- sites/sanmarcosrecord.etypegoogle4.com/themes/md_thenews/js/script.js
+++ sites/sanmarcosrecord.etypegoogle4.com/themes/md_thenews/js/script.js
@@ -50,7 +50,7 @@
 				if (sliderheight > headlineheight) {
 					headlineheight = sliderheight;
 				}
-				$('#slider .blcontent').height(headlineheight);
+				$('#slider .blcontent').height(headlineheight + 200);
 				$('#block-views-headlines-block .blcontent').height(headlineheight - $('#block-views-headlines-block .block-title').outerHeight() - 1);
 			}
 		}
