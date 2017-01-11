46e18b020755a84b69412491d01475da59b65cfe
Charlie Llewellin <charlie@llewellin.net>
2017-01-11 12:51:15 -0600

    image caption css

--- sites/sanmarcosrecord.etypegoogle4.com/themes/md_thenews/templates/node--article.tpl.php
+++ sites/sanmarcosrecord.etypegoogle4.com/themes/md_thenews/templates/node--article.tpl.php
@@ -79,6 +79,7 @@
         // We hide the comments and links now so that we can render them later.
         hide($content['comments']);
         hide($content['links']);
+        hide($content['field_byline_email']);
         //hide($content['field_image']);
         print render($content);
       ?>
