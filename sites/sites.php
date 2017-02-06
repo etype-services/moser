68cf740c2f86ababcda4dadeb53798d671b926ac
Charlie Llewellin <charlie@llewellin.net>
2017-02-06 09:36:24 -0600

    fredericksburg theme

--- sites/sites.php
+++ sites/sites.php
@@ -1,7 +1,7 @@
 <?php
 /* test.etypegoogle4.com */
 // $sites['test.etypegoogle4.com'] = 'cni-theme-test';
-$sites['test.etypegoogle4.com'] = 'cameronrecord.com';
+$sites['test.etypegoogle4.com'] = 'cameronherald.com';
 
 $sites['bosquecountytoday.etypegoogle4.com'] = 'bosquecountytoday.com';
 $sites['bosquecountytoday.com'] = 'bosquecountytoday.com';
