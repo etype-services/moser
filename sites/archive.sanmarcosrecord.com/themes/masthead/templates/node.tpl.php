73db0ae80af8b8357d41f84c7381e83a245d8594
Charlie Llewellin <charlie@llewellin.net>
2017-02-07 12:26:12 -0600

    archive image for san moarcos

--- sites/archive.sanmarcosrecord.com/themes/masthead/templates/node.tpl.php
+++ sites/archive.sanmarcosrecord.com/themes/masthead/templates/node.tpl.php
@@ -8,7 +8,7 @@
   <?php else: ?>
     <?php if ($teaser): ?>
     <div class="field-image">
-      <div class="field-name-field-image"><img class="teaser-default" src="/sites/sanmarcosdailyrecord.com/files/teaser_default.png" /></div>
+      <div class="field-name-field-image"><img class="teaser-default" src="/sites/archive.sanmarcosrecord.com/files/teaser_default.png" /></div>
     </div>
     <?php endif; ?>
 
