04594a0567422045e3834c1b7982900cfa61c415
Charlie Llewellin <charlie@llewellin.net>
2017-03-13 11:46:50 -0500

    add Slider Ad Module

--- sites/all/modules/etype_sliderad/sliderad.js
+++ sites/all/modules/etype_sliderad/sliderad.js
@@ -27,62 +27,7 @@
             var moving = new Array();
             var dir = new Array();
             var mousedover = false;
-//// START PREVIEW OPEN ///////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-            function preview(objname){
-                if(moving[objname])
-                    return;
-                previewdown(objname);  /// uncomment this line as well as the next in order for the ad to expand open on page load and then retract after set time. ////
-                setTimeout("previewup('"+objname+"')", 20000);  //// uncomment the line to the left and adjust the time that the add is expanded on page load. ////
-//// START - SET COOKIE FOR PREVIEW FUNCTION //////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-                //var cookie = getcookie("sbb");
-// alert(cookie);
-                if(cookie)
-                {
-                    document.getElementById(objname).style.display = 'none';
-                }
-                else
-                {
-                    document.getElementById(objname).style.display = 'block';
-                    var today = new Date();
-                    var expire = new Date();
-                    expire.setTime(today.getTime() + 600000);  // currently set for 10 minutes. 600000 milliseconds = 10min.
-                    document.cookie = "sbb=1;expires="+expire.toGMTString();
-                }
-                function getcookie(cookiename) {
-                    var cookiestring=""+document.cookie;
-                    var index1=cookiestring.indexOf(cookiename);
-                    if (index1==-1 || cookiename=="") return "";
-                    var index2=cookiestring.indexOf(';',index1);
-                    if (index2==-1) index2=cookiestring.length;
-                    return unescape(cookiestring.substring(index1+cookiename.length+1,index2));
-                }
-//// END - SET COOKIE FOR PREVIEW FUNCTION //////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-            }
-            function previewdown(objname){
-                if(moving[objname])
-                    return;
-                if(document.getElementById(objname).style.display != "none")
-                    return; // cannot slide down something that is already visible
-                moving[objname] = true;
-                dir[objname] = "down";
-                startslide(objname);
-            }
-            function previewup(objname){
-                if(moving[objname])
-                    return;
-                if(document.getElementById(objname).style.display == "none")
-                    return; // cannot slide up something that is already hidden
-                moving[objname] = true;
-                dir[objname] = "up";
-                startslide(objname);
-            }
-//// END PREVIEW OPEN ///////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
             function slidedown(objname){
                 if(moving[objname])
                     return;
@@ -143,15 +88,12 @@
                 return;
             }
 
-            (function($) {
-                $('#adone').mouseover(function() {
-                    slidedown('adtwo');
-                });
-                $('#adone').mouseout(function() {
-                    slideup('adtwo');
-                });
-            })(jQuery);
-
+            $('#adone').mouseover(function() {
+                slidedown('adtwo');
+            });
+            $('#adone').mouseout(function() {
+                slideup('adtwo');
+            });
 
         }
     };
