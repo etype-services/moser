b164b901ca54936919d3cacc10d56f7d3090b382
Charlie Llewellin <charlie@llewellin.net>
2018-05-03 10:03:39 -0500

    san marcos slideshow

--- sites/sanmarcosrecord.etypegoogle4.com/themes/sanmarcosrecord/js/sanmarcosrecord.js
+++
@@ -1,7 +0,0 @@
-(function ($) {
-    Drupal.behaviors.slideshow = {
-        attach: function (context) {
-            $(".views_slideshow_cycle_slide").css("postion", "relative");
-        }
-    };
-})(jQuery);
