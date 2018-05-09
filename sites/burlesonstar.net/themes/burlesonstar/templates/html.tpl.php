455e81e7a5085a7195a1934ee8dcc9f09790f7ce
Charlie Llewellin <charlie@llewellin.net>
2018-05-09 13:32:17 -0500

    burleson script

--- sites/burlesonstar.net/themes/burlesonstar/templates/html.tpl.php
+++ sites/burlesonstar.net/themes/burlesonstar/templates/html.tpl.php
@@ -6,7 +6,6 @@
     <title><?php print $head_title; ?></title>
     <?php print $styles; ?>
     <?php print $scripts; ?>
-    <link href="https://fonts.googleapis.com/css?family=Anton|Roboto" rel="stylesheet">
     <meta name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1">
     <link href='//fonts.googleapis.com/css?family=Lato' rel='stylesheet'
