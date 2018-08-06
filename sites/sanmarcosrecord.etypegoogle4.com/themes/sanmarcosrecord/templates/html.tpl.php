284d25e7470b78ac5c4dd153e3c967002d0531e7
Charlie Llewellin <charlie@llewellin.net>
2018-08-06 15:07:32 +0100

    san marcos html template

--- sites/sanmarcosrecord.etypegoogle4.com/themes/sanmarcosrecord/templates/html.tpl.php
+++ sites/sanmarcosrecord.etypegoogle4.com/themes/sanmarcosrecord/templates/html.tpl.php
@@ -12,7 +12,7 @@
     <!--[if lt IE 9]>
     <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
-    <?php print $head_script; ?>
+    <?php print $head_script; ?>g
 </head>
 <body class="<?php print $classes; ?>"<?php print $attributes; ?>>
 <div id="fb-root"></div>
