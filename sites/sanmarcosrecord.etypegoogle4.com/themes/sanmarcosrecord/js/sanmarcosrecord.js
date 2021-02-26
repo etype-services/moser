bcaa02d28f2ff698817b6b89deb2c851bb1b1ddf
Charlie Llewellin <charlie@llewellin.net>
2021-02-26 11:49:03 -0500

    Bark Contest Block

---
+++ sites/sanmarcosrecord.etypegoogle4.com/themes/sanmarcosrecord/js/sanmarcosrecord.js
@@ -0,0 +1,9 @@
+(function ($) {
+  Drupal.behaviors.sanMarcosRecord = {
+    attach: function (context) {
+      setTimeout(function () {
+        $("#block-block-69").css('display', 'blovk');
+      }, 10000);
+    }
+  };
+})(jQuery);
